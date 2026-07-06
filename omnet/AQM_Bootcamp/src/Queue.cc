7b73175f0dfc12f711b33e295b375f051c9a19c9
elorsomething <eleenaelizabethsony@gmail.com>
2026-07-06 11:32:28 +0530

    Day 3: Added Queue module and forwarding

--- omnet/AQM_Bootcamp/src/Queue.cc
+++ omnet/AQM_Bootcamp/src/Queue.cc
@@ -3,28 +3,31 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-//
+// 
 
-package aqm_bootcamp;
+#include "Queue.h"
 
-//
-// TODO auto-generated type
-//
-network SimpleNetwork
+Define_Module(Queue);
+
+void Queue::initialize()
 {
-    submodules:
-        generator: Generator;
-        queue: Queue;
-        receiver: Receiver;
-    connections:
-        generator.out --> queue.in;
-        queue.out --> receiver.in;
+    // TODO - Generated method body
+}
+
+void Queue::handleMessage(cMessage *msg)
+{
+    EV << "Queue forwarding: "
+       << msg->getName()
+       << " at "
+       << simTime()
+       << endl;
+    send(msg, "out");
 }
